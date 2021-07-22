b5c64b0aa72b3d80ba01f67d1eccf15fb316c233
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-22 16:32:58 +0200

    sm8250-common: Kill touchfeature
    Change-Id: Iea8dbe1a69906627ffdabca70ecea624435fc53e

--- kona.mk
+++ kona.mk
@@ -174,8 +174,7 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_kona
 
 PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
-    vendor.xiaomi.hardware.touchfeature@1.0.vendor
+    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
