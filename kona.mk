42f09138bd69a7abc9ddc5bb0a12976cfa5a4386
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2021-08-12 17:17:00 +0200

    umi:use stock biometrics

--- kona.mk
+++ kona.mk
@@ -176,8 +176,8 @@ PRODUCT_COPY_FILES += \
 endif
 
 PRODUCT_PACKAGES += \
-    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \ 
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_kona
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+   # android.hardware.biometrics.fingerprint@2.1-service.xiaomi_kona
 
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
