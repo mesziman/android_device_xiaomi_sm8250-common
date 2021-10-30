67116b5fc9ef9c8265afc3531955eefccce65c15
Vishalcj17 <vishalcj@aospa.co>
2021-10-29 22:30:41 -0300

    sm8250-common: Disable ELF Checks
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,6 +67,9 @@ DEVICE_MANIFEST_FILE += \
 
 DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 
+# Hacks
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Kernel
 ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
 BOARD_BOOT_HEADER_VERSION := 3
