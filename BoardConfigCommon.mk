d6f682aa15637f22c0f5b2358529d278cb124131
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2022-08-26 13:08:39 +0200

    umi: Enable LZ4 compression for ramdisks

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,6 +82,7 @@ BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
+BOARD_RAMDISK_USE_LZ4 := true
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
