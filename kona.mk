10e7061ef7e2fb286522a2b0ad6487d08b7d6ce4
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2022-10-01 19:03:22 +0200

    fix manifest

--- kona.mk
+++ kona.mk
@@ -127,8 +127,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor 
  
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
