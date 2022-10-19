df80378981ffd13735c9c793bbfab6d97aaf2518
Akash Srivastava <akashniki@gmail.com>
2022-10-19 19:44:43 +0700

    umi: Add support for misys
    * System looks for it
    10-19 08:53:42.983   616   616 I hwservicemanager: getTransport: Cannot find entry vendor.xiaomi.hardware.misys@1.0::IMiSys/default in either framework or device VINTF manifest.
    10-19 08:53:42.983  5015  5054 W System.err: java.util.NoSuchElementException
    
    # Conflicts:
    #	device.mk
    #	proprietary-files.txt
    #	sepolicy/public/attributes
    #	sepolicy/vendor/file.te
    #	sepolicy/vendor/hal_misys_default.te
    #	sepolicy/vendor/hwservice.te
    #	sepolicy/vendor/hwservice_contexts
    #	sepolicy/vendor/platform_app.te
    #	sepolicy/vendor/vendor_init.te
    #	vintf/vendor_framework_compatibility_matrix.xml
    
    # Conflicts:
    #	kona.mk
    #	sepolicy/public/attributes
    #	sepolicy/vendor/hal_camera_default.te
    #	sepolicy/vendor/hal_misys_default.te
    #	sepolicy/vendor/hwservice.te
    #	sepolicy/vendor/hwservice_contexts
    #	sepolicy/vendor/platform_app.te
    #	sepolicy/vendor/vendor_init.te

--- kona.mk
+++ kona.mk
@@ -150,17 +150,18 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.vendor.bt.a2dp.aac_whitelist=false
 
 
-# Camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
+#Camera
+#PRODUCT_PACKAGES += \
+   android.hardware.camera.provider@2.4-impl \
+   android.hardware.camera.provider@2.4-service_64 \
+   vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libstdc++.vendor \
     libxml2 \
-    libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
+    libdng_sdk.vendor
+#    vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_VENDOR_PROPERTIES += \
