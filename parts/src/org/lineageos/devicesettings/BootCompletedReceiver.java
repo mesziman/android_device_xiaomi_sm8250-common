59891f4ede46c96ca19b3ea44f2c403f7009a322
EmanuelCN <emanuelghub@gmail.com>
2023-05-30 02:11:51 +0530

    umi: parts: Implement DC dimming
    Squash of the following commits:
    - parts: Move dc dimming to Settings > Display
    - parts: Implement dc dimming preference
    - parts: Disable dc dimming if it's not supported
    - parts: Add DC-Dimming QS tile
    - sepolicy: Address DC Dimming denial
    - parts: Add exported flags for sdk32+
    
    Co-Authored-By: Arian <arian.kulmer@web.de>
    
    # Conflicts:
    #	parts/AndroidManifest.xml
    #	parts/res/values/strings.xml
    #	parts/src/org/lineageos/devicesettings/BootCompletedReceiver.java

--- parts/src/org/lineageos/devicesettings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/devicesettings/BootCompletedReceiver.java
@@ -20,7 +20,11 @@ package org.lineageos.devicesettings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
 import android.util.Log;
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.devicesettings.dirac.DiracUtils;
 import org.lineageos.settings.utils.FileUtils;
@@ -28,12 +32,17 @@ import org.lineageos.settings.utils.FileUtils;
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "DeviceParts";
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private static final String DC_DIMMING_NODE = "/sys/class/drm/card0-DSI-1/disp_param";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize(context);
         FileUtils.enableService(context);
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "0x40000" : "0x50000");
     }
 }
