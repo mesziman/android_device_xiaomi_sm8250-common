AOSPA/android_device_xiaomi_surya

surya: parts: Introduce Dolby Atmos
Based on existing dirac implementation and observing stock
sound effects app and daxservice.

Thanks to jhenrique09 for the hack from old dirac parts
"Pause/play music stream to get effects applied".

TODO: bring back misound (same as stock)

Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
Change-Id: I8788709be211b54fd551d3ad0e928a46f5bf9e25
Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -27,6 +27,7 @@ import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.utils.FileUtils;
+import org.lineageos.settings.dolby.DolbyUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
@@ -41,15 +42,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             return;
         }
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
-        try {
-            DiracUtils.getInstance(context);
-        } catch (Exception e) {
-            Log.d(TAG, "Dirac is not present in system");
-        }
         RefreshUtils.initialize(context);
         ThermalUtils.startService(context);
+
+        // Dolby Atmos
+        DolbyUtils.getInstance(context).onBootCompleted();
 
     }
 }

parts/src/org/lineageos/settings/dolby/DolbyActivity.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.dolby;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class DolbyActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_DOLBY = "dolby";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new DolbySettingsFragment(), TAG_DOLBY).commit();
+    }
+}

parts/src/org/lineageos/settings/dolby/DolbyAtmos.java
@@ -1,0 +1,114 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.dolby;
+
+import android.media.audiofx.AudioEffect;
+import android.util.Log;
+
+import java.util.UUID;
+
+public class DolbyAtmos extends AudioEffect {
+
+    private static final String TAG = "DolbyAtmos";
+    private static final UUID EFFECT_TYPE_DAP =
+            UUID.fromString("9d4921da-8225-4f29-aefa-39537a04bcaa");
+    private static final int DAP_PARAM = 5;
+    private static final int DAP_PARAM_PROFILE = 0xA000000;
+    private static final int DAP_PARAM_VALUE = 0x1000000;
+    private static final int DAP_PARAM_GEQ = 110;
+    private static final int DAP_PARAM_VOLUME_LEVELER = 103;
+    private static final int DAP_PROFILES_COUNT = 9;
+
+    public DolbyAtmos(int priority, int audioSession) {
+        super(EFFECT_TYPE_NULL, EFFECT_TYPE_DAP, priority, audioSession);
+    }
+
+    private static int int32ToByteArray(int value, byte[] buf, int offset) {
+        buf[offset] = (byte) (value & 255);
+        buf[offset + 1] = (byte) ((value >>> 8) & 255);
+        buf[offset + 2] = (byte) ((value >>> 16) & 255);
+        buf[offset + 3] = (byte) ((value >>> 24) & 255);
+        return 4;
+    }
+
+    private static int byteArrayToInt32(byte[] buf) {
+        return (buf[0] & 255) | ((buf[3] & 255) << 24)
+                | ((buf[2] & 255) << 16) | ((buf[1] & 255) << 8);
+    }
+
+    private static int int32ArrayToByteArray(int[] values, byte[] buf, int offset) {
+        for (int value : values) {
+            buf[offset] = (byte) ((value >>> 0) & 255);
+            buf[offset + 1] = (byte) ((value >>> 8) & 255);
+            buf[offset + 2] = (byte) ((value >>> 16) & 255);
+            buf[offset + 3] = (byte) ((value >>> 24) & 255);
+            offset += 4;
+        }
+        return values.length << 2;
+    }
+
+    private void setIntParam(int param, int value) {
+        byte[] buf = new byte[12];
+        int i = int32ToByteArray(param, buf, 0);
+        int32ToByteArray(value, buf, i + int32ToByteArray(1, buf, i));
+        checkStatus(setParameter(DAP_PARAM, buf));
+    }
+
+    private int getIntParam(int param) {
+        byte[] buf = new byte[12];
+        int32ToByteArray(param, buf, 0);
+        checkStatus(getParameter(DAP_PARAM + param, buf));
+        return byteArrayToInt32(buf);
+    }
+
+    private void setDapParameter(int param, int values[]) {
+        for (int profile = 0; profile < DAP_PROFILES_COUNT; profile++) {
+            int length = values.length;
+            byte[] buf = new byte[(length + 4) * 4];
+            int i = int32ToByteArray(DAP_PARAM_VALUE, buf, 0);
+            int i2 = i + int32ToByteArray(length + 1, buf, i);
+            int i3 = i2 + int32ToByteArray(profile, buf, i2);
+            int32ArrayToByteArray(values, buf, i3 + int32ToByteArray(param, buf, i3));
+            checkStatus(setParameter(DAP_PARAM, buf));
+        }
+    }
+
+    public void setDsOn(boolean on) {
+        setIntParam(0, on ? 1 : 0);
+        super.setEnabled(on);
+    }
+
+    public boolean getDsOn() {
+        return getIntParam(0) == 1;
+    }
+
+    public void setProfile(int index) {
+        setIntParam(DAP_PARAM_PROFILE, index);
+    }
+
+    public int getProfile() {
+        return getIntParam(DAP_PARAM_PROFILE);
+    }
+
+    public void setGeqBandGains(int[] gains) {
+        setDapParameter(DAP_PARAM_GEQ, gains);
+    }
+
+    public void setVolumeLevelerEnabled(boolean enable) {
+        setDapParameter(DAP_PARAM_VOLUME_LEVELER, new int[]{enable ? 1 : 0});
+    }
+}

parts/src/org/lineageos/settings/dolby/DolbySettingsFragment.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2018,2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.dolby;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Switch;
+
+import androidx.preference.Preference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+import com.android.settingslib.widget.OnMainSwitchChangeListener;
+
+import org.lineageos.settings.R;
+
+public class DolbySettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener, OnMainSwitchChangeListener {
+
+    public static final String PREF_ENABLE = "dolby_enable";
+    public static final String PREF_PRESET = "dolby_preset";
+    public static final String PREF_PROFILE = "dolby_profile";
+
+    private MainSwitchPreference mSwitchBar;
+    private ListPreference mPresetPref;
+    private ListPreference mProfilePref;
+
+    private DolbyUtils mDolbyUtils;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.dolby_settings);
+
+        mDolbyUtils = DolbyUtils.getInstance(getActivity());
+        final boolean dsOn = mDolbyUtils.getDsOn();
+
+        mSwitchBar = (MainSwitchPreference) findPreference(PREF_ENABLE);
+        mSwitchBar.addOnSwitchChangeListener(this);
+        mSwitchBar.setChecked(dsOn);
+
+        mPresetPref = (ListPreference) findPreference(PREF_PRESET);
+        mPresetPref.setOnPreferenceChangeListener(this);
+        mPresetPref.setEnabled(dsOn);
+
+        mProfilePref = (ListPreference) findPreference(PREF_PROFILE);
+        mProfilePref.setOnPreferenceChangeListener(this);
+        mProfilePref.setEnabled(dsOn);
+        mProfilePref.setValue(Integer.toString(mDolbyUtils.getProfile()));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case PREF_PRESET:
+                mDolbyUtils.setPreset(newValue.toString());
+                return true;
+            case PREF_PROFILE:
+                mDolbyUtils.setProfile(Integer.parseInt((newValue.toString())));
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        mSwitchBar.setChecked(isChecked);
+
+        mDolbyUtils.setDsOn(isChecked);
+        mPresetPref.setEnabled(isChecked);
+        mProfilePref.setEnabled(isChecked);
+    }
+}

parts/src/org/lineageos/settings/dolby/DolbyTileService.java
@@ -1,0 +1,34 @@
+package org.lineageos.settings.dolby;
+
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public class DolbyTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+        Tile tile = getQsTile();
+        if (DolbyUtils.getInstance(getApplicationContext()).getDsOn()) {
+            tile.setState(Tile.STATE_ACTIVE);
+        } else {
+            tile.setState(Tile.STATE_INACTIVE);
+        }
+        tile.updateTile();
+        super.onStartListening();
+    }
+
+    @Override
+    public void onClick() {
+        Tile tile = getQsTile();
+        DolbyUtils dolbyUtils = DolbyUtils.getInstance(getApplicationContext());
+        if (dolbyUtils.getDsOn()) {
+            dolbyUtils.setDsOn(false);
+            tile.setState(Tile.STATE_INACTIVE);
+        } else {
+            dolbyUtils.setDsOn(true);
+            tile.setState(Tile.STATE_ACTIVE);
+        }
+        tile.updateTile();
+        super.onClick();
+    }
+}

mesziman/android_device_xiaomi_sm8250-common

parts/src/org/lineageos/devicesettings/BootCompletedReceiver.java
@@ -29,6 +29,7 @@ import androidx.preference.PreferenceManager;
 import org.lineageos.devicesettings.dirac.DiracUtils;
 import org.lineageos.devicesettings.utils.DisplayUtils;
 import org.lineageos.devicesettings.utils.FileUtils;
+import org.lineageos.devicesettings.dolby.DolbyUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -39,12 +40,17 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            return;
+        }
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
-        DiracUtils.initialize(context);
         DisplayUtils.enableService(context);
 
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
         FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
+
+        // Dolby Atmos
+        DolbyUtils.getInstance(context).onBootCompleted();
     }
 }

parts/src/org/lineageos/devicesettings/dolby/DolbyActivity.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.devicesettings.dolby;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class DolbyActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_DOLBY = "dolby";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new DolbySettingsFragment(), TAG_DOLBY).commit();
+    }
+}

parts/src/org/lineageos/devicesettings/dolby/DolbyAtmos.java
@@ -1,0 +1,114 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.devicesettings.dolby;
+
+import android.media.audiofx.AudioEffect;
+import android.util.Log;
+
+import java.util.UUID;
+
+public class DolbyAtmos extends AudioEffect {
+
+    private static final String TAG = "DolbyAtmos";
+    private static final UUID EFFECT_TYPE_DAP =
+            UUID.fromString("9d4921da-8225-4f29-aefa-39537a04bcaa");
+    private static final int DAP_PARAM = 5;
+    private static final int DAP_PARAM_PROFILE = 0xA000000;
+    private static final int DAP_PARAM_VALUE = 0x1000000;
+    private static final int DAP_PARAM_GEQ = 110;
+    private static final int DAP_PARAM_VOLUME_LEVELER = 103;
+    private static final int DAP_PROFILES_COUNT = 9;
+
+    public DolbyAtmos(int priority, int audioSession) {
+        super(EFFECT_TYPE_NULL, EFFECT_TYPE_DAP, priority, audioSession);
+    }
+
+    private static int int32ToByteArray(int value, byte[] buf, int offset) {
+        buf[offset] = (byte) (value & 255);
+        buf[offset + 1] = (byte) ((value >>> 8) & 255);
+        buf[offset + 2] = (byte) ((value >>> 16) & 255);
+        buf[offset + 3] = (byte) ((value >>> 24) & 255);
+        return 4;
+    }
+
+    private static int byteArrayToInt32(byte[] buf) {
+        return (buf[0] & 255) | ((buf[3] & 255) << 24)
+                | ((buf[2] & 255) << 16) | ((buf[1] & 255) << 8);
+    }
+
+    private static int int32ArrayToByteArray(int[] values, byte[] buf, int offset) {
+        for (int value : values) {
+            buf[offset] = (byte) ((value >>> 0) & 255);
+            buf[offset + 1] = (byte) ((value >>> 8) & 255);
+            buf[offset + 2] = (byte) ((value >>> 16) & 255);
+            buf[offset + 3] = (byte) ((value >>> 24) & 255);
+            offset += 4;
+        }
+        return values.length << 2;
+    }
+
+    private void setIntParam(int param, int value) {
+        byte[] buf = new byte[12];
+        int i = int32ToByteArray(param, buf, 0);
+        int32ToByteArray(value, buf, i + int32ToByteArray(1, buf, i));
+        checkStatus(setParameter(DAP_PARAM, buf));
+    }
+
+    private int getIntParam(int param) {
+        byte[] buf = new byte[12];
+        int32ToByteArray(param, buf, 0);
+        checkStatus(getParameter(DAP_PARAM + param, buf));
+        return byteArrayToInt32(buf);
+    }
+
+    private void setDapParameter(int param, int values[]) {
+        for (int profile = 0; profile < DAP_PROFILES_COUNT; profile++) {
+            int length = values.length;
+            byte[] buf = new byte[(length + 4) * 4];
+            int i = int32ToByteArray(DAP_PARAM_VALUE, buf, 0);
+            int i2 = i + int32ToByteArray(length + 1, buf, i);
+            int i3 = i2 + int32ToByteArray(profile, buf, i2);
+            int32ArrayToByteArray(values, buf, i3 + int32ToByteArray(param, buf, i3));
+            checkStatus(setParameter(DAP_PARAM, buf));
+        }
+    }
+
+    public void setDsOn(boolean on) {
+        setIntParam(0, on ? 1 : 0);
+        super.setEnabled(on);
+    }
+
+    public boolean getDsOn() {
+        return getIntParam(0) == 1;
+    }
+
+    public void setProfile(int index) {
+        setIntParam(DAP_PARAM_PROFILE, index);
+    }
+
+    public int getProfile() {
+        return getIntParam(DAP_PARAM_PROFILE);
+    }
+
+    public void setGeqBandGains(int[] gains) {
+        setDapParameter(DAP_PARAM_GEQ, gains);
+    }
+
+    public void setVolumeLevelerEnabled(boolean enable) {
+        setDapParameter(DAP_PARAM_VOLUME_LEVELER, new int[]{enable ? 1 : 0});
+    }
+}

parts/src/org/lineageos/devicesettings/dolby/DolbySettingsFragment.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2018,2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.devicesettings.dolby;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Switch;
+
+import androidx.preference.Preference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+import com.android.settingslib.widget.OnMainSwitchChangeListener;
+
+import org.lineageos.devicesettings.R;
+
+public class DolbySettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener, OnMainSwitchChangeListener {
+
+    public static final String PREF_ENABLE = "dolby_enable";
+    public static final String PREF_PRESET = "dolby_preset";
+    public static final String PREF_PROFILE = "dolby_profile";
+
+    private MainSwitchPreference mSwitchBar;
+    private ListPreference mPresetPref;
+    private ListPreference mProfilePref;
+
+    private DolbyUtils mDolbyUtils;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.dolby_settings);
+
+        mDolbyUtils = DolbyUtils.getInstance(getActivity());
+        final boolean dsOn = mDolbyUtils.getDsOn();
+
+        mSwitchBar = (MainSwitchPreference) findPreference(PREF_ENABLE);
+        mSwitchBar.addOnSwitchChangeListener(this);
+        mSwitchBar.setChecked(dsOn);
+
+        mPresetPref = (ListPreference) findPreference(PREF_PRESET);
+        mPresetPref.setOnPreferenceChangeListener(this);
+        mPresetPref.setEnabled(dsOn);
+
+        mProfilePref = (ListPreference) findPreference(PREF_PROFILE);
+        mProfilePref.setOnPreferenceChangeListener(this);
+        mProfilePref.setEnabled(dsOn);
+        mProfilePref.setValue(Integer.toString(mDolbyUtils.getProfile()));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case PREF_PRESET:
+                mDolbyUtils.setPreset(newValue.toString());
+                return true;
+            case PREF_PROFILE:
+                mDolbyUtils.setProfile(Integer.parseInt((newValue.toString())));
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        mSwitchBar.setChecked(isChecked);
+
+        mDolbyUtils.setDsOn(isChecked);
+        mPresetPref.setEnabled(isChecked);
+        mProfilePref.setEnabled(isChecked);
+    }
+}

parts/src/org/lineageos/devicesettings/dolby/DolbyTileService.java
@@ -1,0 +1,34 @@
+package org.lineageos.devicesettings.dolby;
+
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public class DolbyTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+        Tile tile = getQsTile();
+        if (DolbyUtils.getInstance(getApplicationContext()).getDsOn()) {
+            tile.setState(Tile.STATE_ACTIVE);
+        } else {
+            tile.setState(Tile.STATE_INACTIVE);
+        }
+        tile.updateTile();
+        super.onStartListening();
+    }
+
+    @Override
+    public void onClick() {
+        Tile tile = getQsTile();
+        DolbyUtils dolbyUtils = DolbyUtils.getInstance(getApplicationContext());
+        if (dolbyUtils.getDsOn()) {
+            dolbyUtils.setDsOn(false);
+            tile.setState(Tile.STATE_INACTIVE);
+        } else {
+            dolbyUtils.setDsOn(true);
+            tile.setState(Tile.STATE_ACTIVE);
+        }
+        tile.updateTile();
+        super.onClick();
+    }
+}