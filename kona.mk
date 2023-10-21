51c1b58398b5c51175d2e4f46421fbd61df6d013
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2023-10-21 09:51:52 +0200

    drop miuicamera

--- kona.mk
+++ kona.mk
@@ -501,7 +501,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     dalvik.vm.heapmaxfree=48m
 
 #$(call inherit-product, vendor/xiaomi/cam_meta/cam_meta-vendor.mk)
-$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/products/miuicamera.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/dolbysurya/dolbysurya-vendor.mk)
 
 # Surfaceflinger
