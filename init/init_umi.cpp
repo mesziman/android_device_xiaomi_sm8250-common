05bf9304ed297e8a7fdc3f554edce618b3eb1028
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2023-10-26 15:07:26 +0200

    readd init

---
+++ init/init_umi.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2022 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <vector>
+
+#include <android-base/properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
+
+using android::base::GetProperty;
+
+void property_override(char const prop[], char const value[], bool add = true) {
+    prop_info* pi;
+
+    pi = (prop_info*)__system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void load_umi() {
+    property_override("ro.boot.hardware.sku", "GLOBAL");
+    property_override("ro.product.brand", "Xiaomi");
+    property_override("ro.product.device", "umi");
+    property_override("ro.product.manufacturer", "Xiaomi");
+    property_override("ro.product.model", "Mi 10");
+    property_override("ro.product.name", "umi");
+}
+
+void vendor_load_properties() {
+   load_umi();
+}
